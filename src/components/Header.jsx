87c6033e3598d6fa5cb6fbb830b35e8f48892031
akbar363 <akbarkomolov777@gmail.com>
2025-05-20 09:44:00 +0500

    change language

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import image from "/public/image.png"
 import { HiChevronDown } from "react-icons/hi";
 import { Link } from 'react-router-dom';
+import {useTranslation} from "react-i18next"
 // Akbar
 function Header() {
+  const {t,i18n} = useTranslation()
+  const changeLanguagefn=(e)=>{
+ 
+    i18n.changeLanguage(e.target.value)
+    
+  }
   return (
     <>
       <div className='bg-[#DEEEFC]'>
@@ -19,7 +26,7 @@ function Header() {
             </div>
 
             <div>
-              <p>ЦЕНЫ</p>
+              <p>{t("ЦЕНЫ")}</p>
             </div>
             <div>
               <Link to={"/blog"}>Блог</Link>
@@ -42,7 +49,7 @@ function Header() {
             <h1 className='rounded-4xl border-2 border-blue-600 px-[25px] py-[5px] hover:bg-blue-600 hover:text-white'>регистрация</h1>
 
 
-            <select>
+            <select onChange={(e)=>{changeLanguagefn(e)}}>
               <option value="ru">RU</option>
               <option value="uz">UZ</option>
               <option value="en">EN</option>
